b50ea933ed1c59b65b64a5d6c5df51780eca5cb5
Baldwin Suen <suenbaldwin@gmail.com>
2019-02-18 09:10:10 -0500

    Add files via upload

---
+++ Project/Assets/Scripts/Shooting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject shot;
+    private Transform playerPos;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(shot, playerPos.position, Quaternion.identity);
+        }
+    }
+
+    private void Start()
+    {
+        playerPos = GetComponent<Transform>();
+    }
+}
