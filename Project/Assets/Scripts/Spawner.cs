99088ddbff48549200ae9cb5043291394f2ab597
Andres Santamaria <afsanta@knights.ucf.edu>
2019-02-20 00:21:01 -0500

    created animationas and animator controller for basic skeleton enemy. current animator has transitions from idle to walk based on movement from enemy.cs script. currently, conditional for idle to walk is based on change in x position. need to edit so that animation changes states based on either x or y movement

---
+++ Project/Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public Transform[] spawnSpots;
+    private float timeBtwSpawns;
+    public float startTimeBtwSpawns;
+
+    private void Start()
+    {
+        timeBtwSpawns = startTimeBtwSpawns;
+    }
+
+    private void Update()
+    {
+        if (timeBtwSpawns <= 0)
+        {
+            int randPos = Random.Range(0, spawnSpots.Length); // if crashes, add -1 to spawnspots.length
+            Instantiate(enemy, spawnSpots[randPos].position, Quaternion.identity);
+            timeBtwSpawns = startTimeBtwSpawns;
+        }
+        else
+        {
+            timeBtwSpawns -= Time.deltaTime;
+        }
+    }
+}
