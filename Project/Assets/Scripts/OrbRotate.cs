38f1a4bc6d1ea616420e6b5a40754ab064bbc66a
Andres Santamaria <afsanta@knights.ucf.edu>
2019-02-27 12:55:51 -0500

    added basic tileset palette and tile assets. added grid for demo

---
+++ Project/Assets/Scripts/OrbRotate.cs
@@ -0,0 +1,43 @@
+﻿ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ 
+ public class OrbiterScript : MonoBehaviour {
+ 
+     public GameObject attracter; // Set this to the gameobject that this gameobject will be attracted to 
+ 
+     public float gravityConstant; // Affects strength of gravity
+ 
+     public Vector2 startVelocity; // This will be the starting velocity of our second object (it needs to have velocity in order to orbit)
+ 
+     private Rigidbody2D rb;
+ 
+     private float attracterMass;
+ 
+     // Use this for initialization
+     void Start () {
+        //  attracter = GameObject.Find("player");
+        //  gravityConstant = 1.0;
+        //  startVelocity = new Vector2(5.0, 5.0);
+         rb = this.GetComponent<Rigidbody2D>();
+         attracterMass = attracter.GetComponent<Rigidbody2D>().mass;
+         rb.velocity = startVelocity;
+     }
+     
+     // FixedUpdate is called once per physics update
+     void FixedUpdate () {
+         float distance = Vector2.Distance(this.transform.position, attracter.transform.position); // Distance between us and attracter
+         Vector2 unrotatedForce = (Vector2.right * gravityConstant * attracterMass) / Mathf.Pow(distance, 2); // Magnitude of force due to gravity
+ 
+         // Now we have to rotate that force so it's pointing towards the attracter
+         Vector2 posDifference = attracter.transform.position - this.transform.position; // Difference in position
+         float angleDifference = Mathf.Atan2(posDifference.y, posDifference.x); // Now, difference in angle
+ 
+         // Now we use some trig to rotate the force vector from pointing right to pointing at the attracting object
+         Vector2 rotatedForce = new Vector2(unrotatedForce.x * Mathf.Cos(angleDifference) - unrotatedForce.y * Mathf.Sin(angleDifference),
+                 unrotatedForce.x * Mathf.Sin(angleDifference) + unrotatedForce.y * Mathf.Cos(angleDifference));
+ 
+         // And now we simply add the force to our rigidbody
+         rb.AddForce(rotatedForce);
+     }
+ }
--- Project/Assets/Scripts/Player.cs
+++ Project/Assets/Scripts/Player.cs
@@ -6,19 +6,38 @@ public class Player : MonoBehaviour
 {
     public float speed;
     public int health = 10;
-
+    public float mass;
     private Rigidbody2D rb;
+    private Animator anim;
     private Vector2 moveVelocity;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.mass = mass;
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveVelocity = moveInput.normalized * speed;
+        Debug.Log(moveInput);
+        if(moveInput != Vector2.zero)
+        {
+            anim.SetFloat("FaceX", moveInput.x);
+            anim.SetFloat("FaceY", moveInput.x);
+            Debug.Log(moveInput);
+            anim.Play("Walk");
+        } else {
+            anim.SetFloat("FaceX", Vector2.zero.x);
+            anim.SetFloat("FaceY", Vector2.zero.y);
+            Debug.Log(moveInput);
+            anim.Play("Idle");
+        }
+
+
+
     }
 
     private void FixedUpdate()
